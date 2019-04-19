398a75f4f9f241add4088e06d992f93b35ed873f
takanobu726 <satotakanobu@satoutakashisennoMacBook-Pro.local>
2019-04-19 10:12:41 +0900

    User管理機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :users_groups
   has_many :groups, through: :users_groups
   has_many :messages
