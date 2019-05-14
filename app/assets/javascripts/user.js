9e60575ef0fd5a9af40ae57907198bb0a159b1a2
takanobu726 <satotakanobu@satoutakashisennoMacBook-Pro.local>
2019-05-14 17:17:17 +0900

    カスタムデータ属性の値取得の記述を変更

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -59,8 +59,8 @@ $(function(){
   }
 
   $(document).on('click', '.user-search-add', function() {
-    var name = $(this).attr('data-user-name')
-    var user_id = $(this).attr('data-user-id')
+    var name = $(this).data('user-name')
+    var user_id = $(this).data('user-id')
     appendUserToGroup(name, user_id)
     $(this).parent().remove()
   });
