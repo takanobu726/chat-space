2bb42f670c67413a7c2acf103b991bd1847fbe8d
takanobu726 <satotakanobu@satoutakashisennoMacBook-Pro.local>
2019-05-17 20:58:23 +0900

    不要なコメントアウトの削除

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,6 +1,5 @@
 class Api::MessagesController < ApplicationController
 
-  # before_action :set_group
 
   def index
     respond_to do |format|
@@ -8,13 +7,5 @@ class Api::MessagesController < ApplicationController
       format.json {@messages = Message.where('id > ?',params[:id]) }
     end
   end
-
-  # def message_params
-  # params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
-  # end
-
-  # def set_group
-  #   @group = Group.find(params[:id])
-  # end
 
 end
