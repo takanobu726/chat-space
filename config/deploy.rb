d05dec55f0f05568cd4ef19f5d4ca71c53ce122d
takanobu726 <satotakanobu@satoutakashisennoMacBook-Pro.local>
2019-05-27 18:24:13 +0900

    branchの指定

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,7 @@ lock "3.11.0"
 
 set :application, "chat-space"
 set :repo_url, "git@github.com:takanobu726/chat-space.git"
+set :branch, 'master'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
