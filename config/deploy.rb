f1997f62cd4c4326d7b25843bad3bd31029b6f50
takanobu726 <satotakanobu@satoutakashisennoMacBook-Pro.local>
2019-05-27 18:36:44 +0900

    ssh_options記述変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,9 +10,9 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
-set :ssh_options, auth_methods: ['publickey'],
+set :ssh_options, { auth_methods: ['publickey'],
                   keys: [File.expand_path('~/.ssh/kickass.pem')]
-
+}
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
